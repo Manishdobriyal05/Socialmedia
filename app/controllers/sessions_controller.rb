45a785bd4e9a8653bd92c91973067860d8a28218
Manish <Manish.codenote@gmail.com>
2022-11-15 14:23:06 +0530

    commit sent

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,9 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def login
+  end
+
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
@@ -11,9 +14,6 @@ class SessionsController < ApplicationController
     else
       redirect_to '/login'
     end
-  end
-
-  def login
   end
 
   def welcome
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:new, :create]
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'welcome', to: 'sessions#welcome'
-  get 'authorized', to: 'sessions#page_requires_login'
+  #get 'login', to: 'sessions#new'
+  #post 'login', to: 'sessions#create'
+  #get 'welcome', to: 'sessions#welcome'
+  #get 'authorized', to: 'sessions#page_requires_login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :sessions, only: [:new, :create, :login, :welcome]
+
 end
